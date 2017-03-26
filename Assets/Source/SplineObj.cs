2b1b05db8a5a07c4a09b56a0ad5099b4bd48f1f1
Tyler Beaupre <tylerbeaupre4@gmail.com>
2017-03-26 06:52:25 -0400

    Adding in Control Points with prefabs

---
+++ Assets/Source/SplineObj.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineObj : MonoBehaviour {
+	public GameObject controlPoint;
+	Spline spline;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	void DrawControlPoints()
+	{
+		for (int i = 0; i < spline.poss.Count; i++)
+		{
+			GameObject controlPointClone = Object.Instantiate(controlPoint, spline.poss[i], new Quaternion());
+		}
+	}
+}
